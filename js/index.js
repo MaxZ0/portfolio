e94738c0552e96bac9ceadb3e8e3a6755f5a78ac
Max <55975910+MaxZ0@users.noreply.github.com>
2021-01-24 03:58:21 +0100

    making a hidden form in the button
    why i'am doing this? It's to keep the minimalistik and go right to the point. and it feel cool to make one i guess.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var workimages = document.querySelectorAll("#image figure");
 for (i = 0; i < workimages.length; i++) {
     workimages[i].addEventListener('click', function () { this.classList.toggle("expanded"); image.classList.toggle("full") });
-}
+};
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -12,3 +12,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
