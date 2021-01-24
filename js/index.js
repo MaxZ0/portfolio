94be6629e342b64e3696b539327772dc60400d32
Max <55975910+MaxZ0@users.noreply.github.com>
2021-01-24 23:28:14 +0100

    done for now fixet abitt made contact
    i took away my mail bc of spam bot. (dont want to have em sending to my everyting mail)

--- js/index.js
+++ js/index.js
@@ -13,11 +13,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-};
