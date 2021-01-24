8c5f9c2fac933e7669eb1aa049c5989c06eca904
Max <55975910+MaxZ0@users.noreply.github.com>
2021-01-24 02:52:55 +0100

    smoother animations

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@ var workimages = document.querySelectorAll("#image figure");
 for (i = 0; i < workimages.length; i++) {
     workimages[i].addEventListener('click', function () { this.classList.toggle("expanded"); image.classList.toggle("full") });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
