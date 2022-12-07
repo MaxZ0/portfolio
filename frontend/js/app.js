a4f700d2e89dd3fc056f3ee8ae16b4c2b81788bc
Max <55975910+MaxZ0@users.noreply.github.com>
2022-12-07 17:28:54 +0100

    jhgvgku

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -21,83 +21,80 @@ const queryProjectsGalleri = `
 }
 `;
 
-
-
-
-
 async function getProjectGalleri() {
-    const response = await fetch(`${sanityUrl}${encodeURI(queryProjectsGalleri)}`);
-    const { result } = await response.json();
-
-};
+  const response = await fetch(
+    `${sanityUrl}${encodeURI(queryProjectsGalleri)}`
+  );
+  const { result } = await response.json();
+}
 getProjectGalleri();
 
-
 async function getGalleri() {
-    const response = await fetch(`${sanityUrl}${encodeURI(queryProjectsGalleri)}`);
-    const { result } = await response.json();
+  const response = await fetch(
+    `${sanityUrl}${encodeURI(queryProjectsGalleri)}`
+  );
+  const { result } = await response.json();
 
- 
-    const projectsEl = document.querySelector('.gallery-cards');
+  const projectsEl = document.querySelector(".gallery-cards");
 
-    result.forEach(project => {
-     const cardEl = document.createElement('a');
-     cardEl.classList.add('flex');
-     cardEl.classList.add('galleri-container');
-     cardEl.classList.add('grow-rotate-on-hover');
-     cardEl.setAttribute('href',`/projects/?${project.slug.current}`);
+  result.forEach((project) => {
+    const cardEl = document.createElement("a");
+    cardEl.classList.add("flex");
+    cardEl.classList.add("galleri-container");
+    cardEl.classList.add("grow-rotate-on-hover");
+    cardEl.setAttribute("href", `/projects/?${project.slug.current}`);
 
-     const cardImgEl = document.createElement('img');
-     cardImgEl.setAttribute('src', project.mainImage);
-     cardImgEl.setAttribute('style', 'width: 208px; height: 144px; object-fit: cover;');
-     cardEl.append(cardImgEl);
-     projectsEl.append(cardEl);
-    })    
- }
- 
- getGalleri();
+    const cardImgEl = document.createElement("img");
+    cardImgEl.setAttribute("src", project.mainImage);
+    cardImgEl.setAttribute(
+      "style",
+      "width: 208px; height: 144px; object-fit: cover;"
+    );
+    cardEl.append(cardImgEl);
+    projectsEl.append(cardEl);
+  });
+}
 
- /* result */
-async function renderSingelProject(){
-    const response = await fetch(`${sanityUrl}${encodeURI(queryProjectsGalleri)}`);
-    const { result } = await response.json();
-    const titleEl = document.querySelector('.project-title');
-    titleEl.textContent = result[0].title;
-    const coverProjectEl = document.querySelector('.project-cover');
-    coverProjectEl.setAttribute('src', result[0].cover);
+getGalleri();
 
-    handleParagraphs(result[0].process, 'processContent');
-    handleParagraphs(result[0].coreproblem, 'core-problem');
+/* result */
+async function renderSingelProject() {
+  const response = await fetch(
+    `${sanityUrl}${encodeURI(queryProjectsGalleri)}`
+  );
+  const { result } = await response.json();
+
+  const titleEl = document.querySelector(".project-title");
+  titleEl.textContent = result[0].title;
+
+  handleParagraphs(result[0].process, "processContent");
+  handleParagraphs(result[0].coreproblem, "core-problem");
 }
 
 renderSingelProject();
 
-
-
-
 async function getAllProject() {
-    const response = await fetch(`${sanityUrl}${encodeURI(queryAllProjects)}`);
-    const { result } = await response.json();
- 
-    const projectsEl = document.querySelector('.slideshow-container');
+  const response = await fetch(`${sanityUrl}${encodeURI(queryAllProjects)}`);
+  const { result } = await response.json();
 
-    result.forEach(project => {
-     const cardEl = document.createElement('div');
-     cardEl.classList.add('mySlides');
-     cardEl.classList.add('fade');
+  const projectsEl = document.querySelector(".slideshow-container");
 
-     const cardImgEl = document.createElement('img');
-     cardImgEl.setAttribute('src', project.logo);
-     cardImgEl.setAttribute('style', 'width: 64px', 'margin-right: 32px');
-     cardEl.append(cardImgEl);
-     projectsEl.append(cardEl);
-    })    
- }
- 
+  result.forEach((project) => {
+    const cardEl = document.createElement("div");
+    cardEl.classList.add("mySlides");
+    cardEl.classList.add("fade");
+
+    const cardImgEl = document.createElement("img");
+    cardImgEl.setAttribute("src", project.logo);
+    cardImgEl.setAttribute("style", "width: 64px", "margin-right: 32px");
+    cardEl.append(cardImgEl);
+    projectsEl.append(cardEl);
+  });
+}
 
 /* minimerer api request */
-if(urlString === undefined) {
-    getAllProject();
+if (urlString === undefined) {
+  getAllProject();
 }
 
 const querySingleProject = `
@@ -112,39 +109,30 @@ const querySingleProject = `
 `;
 
 async function getProject() {
-    const response = await fetch(`${sanityUrl}${encodeURI(querySingleProject)}`);
-    const { result } = await response.json();
-    console.log(result,"helllllooooooooo");
+  const response = await fetch(`${sanityUrl}${encodeURI(querySingleProject)}`);
+  const { result } = await response.json();
+  console.log(result, "helllllooooooooo");
 
-    const coverEl = document.querySelector('.cover-project');
-    coverEl.setAttribute('src', result[0].mainImage);
-    const projectTitleEl = document.querySelector('.project-title');
-    projectTitleEl.classList.add('hind');
-    projectTitleEl.textContent = result[0].title;
+  const coverEl = document.querySelector(".cover-project");
+  coverEl.setAttribute("src", result[0].mainImage);
+  const projectTitleEl = document.querySelector(".project-title");
+  projectTitleEl.classList.add("hind");
+  projectTitleEl.textContent = result[0].title;
 
-
-    console.log(result[0].body);
-    handleParagraphs(result[0].body, "cover-p")
+  console.log(result[0].body);
+  handleParagraphs(result[0].body, "cover-p");
 }
-
-
-
 
 function renderSingleProject(result) {
-    const titleEl = document.querySelector('.single-project__title');
-    titleEl.textContent = result[0].title;
-    const coverProjectEl = document.querySelector('.project__cover');
-    coverProjectEl.setAttribute('src', result[0].cover);
-    
-    
-    handleParagraphs(result[0].process, 'processContent');
-    handleParagraphs(result[0].coreproblem, 'core-problem');
+  const titleEl = document.querySelector(".single-project__title");
+  titleEl.textContent = result[0].title;
+  const coverProjectEl = document.querySelector(".project__cover");
+  coverProjectEl.setAttribute("src", result[0].cover);
 
-  }
-
-if (urlString !== undefined) {
-    getProject();
+  handleParagraphs(result[0].process, "processContent");
+  handleParagraphs(result[0].coreproblem, "core-problem");
 }
 
-
- 
+if (urlString !== undefined) {
+  getProject();
+}
